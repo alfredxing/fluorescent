d59831824cdf57cf1afcd72089318acbf51bdf70
tyangliu <tyangliu@gmail.com>
2015-05-03 11:38:33 -0700

    refactor css with compass, implement right-push settings menu

---
+++ app/assets/scripts/settings-menu.js
@@ -0,0 +1,9 @@
+(function(window) {
+    var settingsButton = document.querySelector(".fl-settings-icon");
+        settingsMenu = document.querySelector(".settings-menu");
+
+    settingsButton.addEventListener("click", function() {
+        classie.toggle(settingsButton, "open");
+        classie.toggle(settingsMenu, "open");
+    })
+})( window );
