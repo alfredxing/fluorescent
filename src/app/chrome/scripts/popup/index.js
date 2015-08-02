00733404ead037cde2e2746fe5b3c97e731d5082
tyangliu <tyangliu@gmail.com>
2015-08-02 00:34:14 -0700

    Remove dom loaded wrapper in popup script (not needed and slowing load times)

--- src/app/chrome/scripts/popup/index.js
+++ src/app/chrome/scripts/popup/index.js
@@ -2,15 +2,12 @@
 
 chrome.promise = new ChromePromise();
 
-document.addEventListener("DOMContentLoaded", () => {
-  let button = document.getElementById('click-me');
+let button = document.getElementById('click-me');
 
-  button.addEventListener('click', () =>
-    chrome.promise.tabs.query({
-      currentWindow: true,
-      active: true
-    })
-    .then(tabs => chrome.tabs.sendMessage(tabs[0].id, { type: 'uncap' }))
-  );
-
-});
+button.addEventListener('click', () =>
+  chrome.promise.tabs.query({
+    currentWindow: true,
+    active: true
+  })
+  .then(tabs => chrome.tabs.sendMessage(tabs[0].id, { type: 'uncap' }))
+);
